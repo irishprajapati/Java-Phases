e2878866295dcd9a146bbf3a7728d7fb44c74090
Erish <erish968@gmail.com>
2026-02-03 22:03:22 +0545

    method description

---
+++ OOP1/methods.java
@@ -0,0 +1,14 @@
+class facebookAccount{
+    private String username;
+    private int Age;
+    private String phoneNumber;
+    private String location;
+    private String gender;
+
+    public void setUsername(String name){
+        username = name;
+    }
+    public void setAge(String age){
+        Age = age;
+    }
+}
