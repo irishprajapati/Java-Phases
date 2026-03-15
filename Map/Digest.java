24729e03d9d7be1c27401074a77ad040e0968d5a
Erish <erish968@gmail.com>
2026-03-15 12:47:15 +0545

    Refactor: Hashmap code learned

---
+++ Map/Digest.java
@@ -0,0 +1,22 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Digest {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "anish");
+        map.put(2,"subham");
+        map.put(3, "harry");
+//        System.out.println(map);
+//        String student1 = map.get(3);
+//        System.out.println(student1);
+
+
+//        for( int i: map.keyset()){
+//            System.out.println(map.get(i));
+//        }
+
+    }
+}
